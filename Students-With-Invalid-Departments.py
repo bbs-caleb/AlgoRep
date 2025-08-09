32443f8eb62fee9baf0f0c8bea90851b45f593d3
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-08-09 17:55:23 +0500

    [1350] [Time Beats: 7.50%] - LeetPush

---
+++ Students-With-Invalid-Departments.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_students(departments: pd.DataFrame, students: pd.DataFrame) -> pd.DataFrame:
+    invalid = (students
+            .merge(departments, left_on='department_id', right_on='id', how='left', indicator=True)
+            .query('_merge == "left_only"')[['id_x', 'name_x']]
+            .rename(columns={'id_x': 'id', 'name_x': 'name'}))
+    return invalid
