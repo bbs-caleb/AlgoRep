ad770e4db39fab43ae4c9bcf5cb80a2ecb54272f
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-08-11 06:23:04 +0500

    [511] [Time Beats: 69.45%] - LeetPush

--- Game-Play-Analysis-I.py
+++ Game-Play-Analysis-I.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    return activity.groupby('player_id', as_index=False)['event_date'].min().rename(columns={'event_date': 'first_login'})
+    out = (activity.sort_values(['player_id', 'event_date'])
+             .drop_duplicates(subset='player_id', keep='first')
+             [['player_id', 'event_date']]
+             .rename(columns={'event_date': 'first_login'}))
+    return out
