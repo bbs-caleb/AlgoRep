0f3ad9c87d61016aa16a8af2013eee65bf3d8f16
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-08-16 11:22:03 +0500

    Time: 315 ms (60.28%) | Memory: 25.6 MB (71.93%) - LeetSync

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -1,12 +1,19 @@
 class Solution:
     def getTargetCopy(self, original: 'TreeNode', cloned: 'TreeNode', target: 'TreeNode') -> 'TreeNode':
-        stack = [(original, cloned)]
+        from collections import deque 
+
+        if not original:
+            return None
+        
+        stack = deque([(original, cloned)])
         while stack:
-            o, c = stack.pop()
-            if o is None:
-                continue
+            o, c = stack.popleft()
             if o is target:
                 return c
-            stack.append((o.left, c.left))
-            stack.append((o.right, c.right))
-        return None  
+            else:
+                if o.left:
+                    stack.append((o.left, c.left))
+                if o.right:
+                    stack.append((o.right, c.right))
+        
+        
