faae060afcce07aa5d925a18a0f81efedc3c05e4
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-08-17 11:59:06 +0500

    Time: 3 ms (47.57%) | Memory: 18 MB (67.60%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -7,26 +7,24 @@
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         from collections import deque
-
         if not root1: return root2
         if not root2: return root1
 
-        q = deque([(root1, root2)])
+        stack = deque([(root1, root2)])
 
-        while q:
-            u, v = q.popleft()
-            u.val += v.val
+        while stack:
+            u, v = stack.popleft()
+            u.val += v.val 
 
             if not u.left:
-                u.left = v.left 
+                u.left = v.left
             else:
                 if v.left:
-                    q.append((u.left, v.left))
-            
+                    stack.append((u.left, v.left))
+
             if not u.right:
                 u.right = v.right
             else:
                 if v.right:
-                    q.append((u.right, v.right))
-        
+                    stack.append((u.right, v.right))
         return root1 
