f7e0c2b2934081d59f66e3a3605d540e71ad6776
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-08-17 18:07:08 +0500

    [589] [Time Beats: 43.51%] [Memory Beats: 93.96%] - LeetPush

---
+++ N-ary-Tree-Preorder-Traversal.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        stack = [root]
+        ans = []
+
+        while stack:
+            node = stack.pop()
+            ans.append(node.val)
+
+            stack.extend(node.children[::-1])
+        return ans 
