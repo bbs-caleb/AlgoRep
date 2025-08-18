d86c691c73cf75fb573e3fc6f204992e9bfd6971
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-08-18 22:42:20 +0500

    Time: 675 ms (18.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1718-warehouse-manager/warehouse-manager.sql
@@ -0,0 +1,7 @@
+SELECT 
+    w.name AS warehouse_name,
+    SUM(w.units * p.Width * p.Length * p.Height) AS volume
+FROM Warehouse w
+JOIN Products p
+    ON w.product_id = p.product_id
+GROUP BY w.name;
