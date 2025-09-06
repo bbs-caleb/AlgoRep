131b3c9b14a28b8b3caf40c8230110c700f025c0
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-09-06 20:58:30 +0500

    [569] [Time Beats: 55.62%] - LeetPush

---
+++ Median-Employee-Salary.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+SELECT id, company, salary
+FROM (
+  SELECT
+    id,
+    company,
+    salary,
+    ROW_NUMBER() OVER (PARTITION BY company ORDER BY salary, id) AS rn,
+    COUNT(*) OVER (PARTITION BY company) AS n
+  FROM Employee
+) t
+WHERE rn IN ( (n + 1) / 2, (n + 2) / 2 )
