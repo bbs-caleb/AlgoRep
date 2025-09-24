84f01c1264327c8dc12ef48958f71c8961ef8930
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-09-25 04:21:01 +0500

    [1350] [Time Beats: 93.59%] - LeetPush

--- Students-With-Invalid-Departments.py
+++ Students-With-Invalid-Departments.py
@@ -1,15 +1,6 @@
 import pandas as pd
 
 def find_students(departments: pd.DataFrame, students: pd.DataFrame) -> pd.DataFrame:
-    merged = students.merge(
-        departments,
-        left_on='department_id',
-        right_on='id',
-        how='left',
-        indicator=True
-    )
-
-    output = merged.loc[merged['_merge']=='left_only'].rename(columns={'id_x':'id', 'name_x': 'name'})[['id', 'name']]
-    return output
-
-    
+    dep_ids = pd.Index(departments['id']).unique()
+    invalid = ~students['department_id'].isin(dep_ids)
+    return students.loc[invalid, ['id', 'name']]
