f0b73632ace074f7b021b8ce66a66772eda35c13
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-10-17 08:19:08 +0500

    Time: 204 ms (61.74%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,19 +1,6 @@
-with cte as (
-    select distinct salary
-    from Employee)
-
-    , cte_2 as (
-        select salary, row_number() over (order by salary desc) as rnk
-        from cte 
-    )
-
-select salary as SecondHighestSalary
-from cte_2
-where rnk = 2 
-
-union all
-
-select Null 
-
-order by SecondHighestSalary nulls last 
-limit 1
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
