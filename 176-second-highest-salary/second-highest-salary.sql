889210846c871c6e0a1a42d1913a92755492f845
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-10-17 08:23:52 +0500

    Time: 188 ms (99.30%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,11 @@
 SELECT (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
+    SELECT salary
+    FROM (
+        SELECT 
+            salary,
+            DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+        FROM Employee
+    ) AS ranked_salaries
+    WHERE rnk = 2
+    LIMIT 1 -- Ensures a single value is returned
 ) AS SecondHighestSalary;
