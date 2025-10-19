e926f80415544e93ea68d84579a6d6c0d3dfccdb
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-10-19 05:32:37 +0500

    Time: 462 ms (98.47%) | Memory: 0B (100.00%) - LeetSync

--- 1180-game-play-analysis-ii/game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/game-play-analysis-ii.sql
@@ -1,11 +1,3 @@
- with user_min_time as (
-    select player_id, min(event_date) as min_dt
-    from Activity
-    group by player_id
- )
-
-select u.player_id, a.device_id 
-from user_min_time u 
-inner join Activity a 
-    on u.player_id = a.player_id and u.min_dt = a.event_date
-    
+SELECT DISTINCT ON (player_id) player_id, device_id
+FROM Activity
+ORDER BY player_id, event_date;
