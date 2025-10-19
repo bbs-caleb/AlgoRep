9d0e9b5213dbd1d25e929c74a6b2e582d2a70fb7
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-10-19 06:02:10 +0500

    Time: 210 ms (50.75%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,16 +1,7 @@
-with requests as (
-    select sender_id, send_to_id
-    from FriendRequest
-    group by 1, 2 
-)
-
-    , accepted as (
-        select requester_id, accepter_id
-        from RequestAccepted
-        group by 1, 2 
-    )
-
-select case when (select count(*) from requests) = 0 then 0.00
-        else round((select count(*) from accepted) * 1.0 / (select count(*) from requests), 2)
-        
-        end as accept_rate
+SELECT
+  CASE WHEN reqs.total_requests = 0 THEN 0.00
+       ELSE ROUND(accs.total_accepts::numeric / reqs.total_requests, 2)
+  END AS accept_rate
+FROM
+  (SELECT COUNT(DISTINCT (sender_id, send_to_id)) AS total_requests FROM FriendRequest) reqs,
+  (SELECT COUNT(DISTINCT (requester_id, accepter_id)) AS total_accepts FROM RequestAccepted) accs;
