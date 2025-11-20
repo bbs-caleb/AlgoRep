6eff67ef596f42fd6d00c2acd297ddac32dce917
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-11-20 21:29:15 +0500

    [2066] [Time Beats: 22.50%] - LeetPush

---
+++ Account-Balance.sql
@@ -0,0 +1,3 @@
+select account_id, day, sum(case when type = 'Deposit' then amount else -amount end) over (partition by account_id order by day) as balance
+from Transactions 
+order by account_id, day 
