29602539746cba5737f87aab951897fedbcb2f37
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-06 14:34:19 +0500

    [1379] [Time Beats: 32.75%] [Memory Beats: 48.90%] - LeetPush

--- Find-a-Corresponding-Node-of-a-Binary-Tree-in-a-Clone-of-That-Tree.py
+++ Find-a-Corresponding-Node-of-a-Binary-Tree-in-a-Clone-of-That-Tree.py
@@ -1,19 +1,25 @@
-class Solution:
-    def getTargetCopy(self, original: 'TreeNode', cloned: 'TreeNode', target: 'TreeNode') -> 'TreeNode':
-        from collections import deque 
-
-        if not original:
-            return None
-        
-        stack = deque([(original, cloned)])
-        while stack:
-            o, c = stack.popleft()
-            if o is target:
-                return c
-            else:
-                if o.left:
-                    stack.append((o.left, c.left))
-                if o.right:
-                    stack.append((o.right, c.right))
-        
-        
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.left = None
+6#         self.right = None
+7
+8class Solution:
+9    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+10        def dfs(node_orig, node_clon):
+11            if node_orig is None:
+12                return None
+13            
+14            if node_orig is target:
+15                return node_clon
+16            
+17            left_res =  dfs(node_orig.left, node_clon.left)
+18            if left_res is not None:
+19                return left_res 
+20            
+21            right_res = dfs(node_orig.right, node_clon.right)
+22            if right_res is not None:
+23                return right_res 
+24            
+25        return dfs(original, cloned)
