ae66cebd3573629fa1411be53de4ea0327a86c6f
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-06 13:17:43 +0500

    [938] [Time Beats: 100.00%] [Memory Beats: 22.12%] - LeetPush

--- Range-Sum-of-BST.py
+++ Range-Sum-of-BST.py
@@ -1,20 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        res = []
-        def dfs(node):
-            if node is None:
-                return
-            dfs(node.left)
-            res.append(node.val)
-            dfs(node.right)
-        
-        dfs(root)
-        l = bisect_left(res, low)
-        h = bisect_right(res, high)
-        return sum(res[l:h])
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+9        if root is None:
+10            return 0
+11        total = 0
+12        
+13        if low <= root.val <= high:
+14            total += root.val
+15        
+16        if root.val > low:
+17            total += self.rangeSumBST(root.left, low, high)
+18        if root.val < high:
+19            total += self.rangeSumBST(root.right, low, high)
+20        return total 
+21
+22
