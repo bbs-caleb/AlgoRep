53f64692d77624402fcead3575b6c6b9f1958b3c
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-07 19:31:07 +0500

    [100] [Time Beats: 100.00%] [Memory Beats: 20.44%] - LeetPush

--- Same-Tree.py
+++ Same-Tree.py
@@ -1,16 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-        return (p.val == q.val and
-                self.isSameTree(p.left, q.left) and
-                self.isSameTree(p.right, q.right))
-        
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7
+8class Solution:
+9    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+10        queue = deque([(p, q)])
+11        while queue:
+12            n1, n2 = queue.popleft()
+13
+14            if not n1 and not n2:
+15                continue
+16            if not n1 or not n2:
+17                return False
+18            if n1.val != n2.val:
+19                return False
+20            
+21            queue.append((n1.left, n2.left))
+22            queue.append((n1.right, n2.right))
+23        return True 
+24
+25
