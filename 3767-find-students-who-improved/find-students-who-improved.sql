12b1ba287a2cbb1c64657a54b3c2889a4261242c
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-08 11:32:10 +0500

    Time: 191 ms (44.76%) | Memory: 0B (100.00%) - LeetSync

--- 3767-find-students-who-improved/find-students-who-improved.sql
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -1,24 +1,25 @@
+
 with cte as (
-    select student_id, subject, min(exam_date) as first_date, max(exam_date) as last_date
-    from Scores 
-    group by 1, 2 
-    having min(exam_date::date) != max(exam_date::date)
-)
+select  
+        student_id,
+        subject,
+        first_value(score) over (
+            partition by student_id, subject
+            order by exam_date::date
+            rows between unbounded preceding and unbounded following
+        ) as first_score,
+        last_value(score) over (
+            partition by student_id, subject 
+            order by exam_date::date
+            rows between unbounded preceding and unbounded following
+        ) as latest_score
+from Scores ) 
 
-    , cte_2 as (
-        select  s.student_id, 
-                s.subject, 
-                sum(case when s.exam_date = c.first_date then s.score else 0 end) as first_score, 
-                sum(case when s.exam_date = c.last_date then s.score else 0 end) as latest_score
-        from Scores s 
-        inner join cte c 
-            on s.student_id = c.student_id 
-                and s.subject = c.subject
-        group by 1, 2
-        having sum(case when s.exam_date = c.last_date then s.score else 0 end) > sum(case when s.exam_date = c.first_date then s.score else 0 end)
-
-
-    )
-select * 
-from cte_2
-order by student_id, subject 
+select  student_id, 
+        subject,
+        first_score, 
+        latest_score
+from cte 
+where first_score < latest_score
+group by 1, 2, 3, 4
+order by student_id, subject;
