1bf31453d19af562cb51e53fe8b6b86ae4b27137
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-08 12:19:22 +0500

    Time: 208 ms (98.68%) | Memory: 0B (100.00%) - LeetSync

--- 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -1,19 +1,13 @@
-with borrowed as (
-    select  book_id,
-            count(*) as total 
-    from borrowing_records 
-    where return_date is null
-    group by 1 
-)
 select  
         lb.book_id,
         lb.title,
         lb.author,
-        lb.genre, 
+        lb.genre,
         lb.publication_year,
-        b.total as current_borrowers
+        lb.total_copies as current_borrowers
 from library_books lb 
-inner join borrowed b 
-        on lb.book_id = b.book_id
-                and lb.total_copies = b.total
+inner join borrowing_records b 
+    on lb.book_id = b.book_id
+group by 1, 2, 3, 4, 5, 6
+having lb.total_copies = sum(case when return_date is null then 1 else 0 end)
 order by current_borrowers desc, lb.title;
