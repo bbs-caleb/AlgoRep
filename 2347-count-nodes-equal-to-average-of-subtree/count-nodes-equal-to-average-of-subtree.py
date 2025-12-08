62bd93b22a3b67a03e2fe64db7f113144bdb7cf9
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-08 17:06:28 +0500

    Time: 53 ms (13.03%) | Memory: 17.8 MB (99.02%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -6,17 +6,18 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
-        self.answer = 0
+        if not root:
+            return 0
 
-        def dfs(node): 
-            if node is None:
+        self.answer = 0
+        def dfs(node):
+            if not node:
                 return 0, 0
 
             left_sum, left_count = dfs(node.left)
             right_sum, right_count = dfs(node.right)
-
             total_sum = left_sum + right_sum + node.val
-            total_count = left_count + right_count + 1
+            total_count = left_count + right_count + 1 
 
             avg = total_sum // total_count
 
@@ -24,6 +25,5 @@ class Solution:
                 self.answer += 1 
             
             return total_sum, total_count
-        
         dfs(root)
         return self.answer 
