123bccc8e527b6dbf6f4e46401b3a13edde01714
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-09 08:29:45 +0500

    [1303] [Time Beats: 45.00%] - LeetPush

--- Find-the-Team-Size.py
+++ Find-the-Team-Size.py
@@ -1,9 +1,5 @@
-import pandas as pd
-
-def team_size(employee: pd.DataFrame) -> pd.DataFrame:
-    employee['team_size'] = employee.groupby('team_id')['employee_id'].transform('size')
-    return employee[['employee_id', 'team_size']]
-
-    
-
-    
+1import pandas as pd
+2
+3def team_size(employee: pd.DataFrame) -> pd.DataFrame:
+4    employee['team_size'] = employee.groupby('team_id')['employee_id'].transform('size')
+5    return employee[['employee_id', 'team_size']]
