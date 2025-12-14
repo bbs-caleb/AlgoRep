905152b6e9a2dc87f9ec500ebca37263c1233491
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-14 13:42:29 +0500

    Time: 349 ms (6.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3711-first-letter-capitalization-ii/first-letter-capitalization-ii.sql
@@ -0,0 +1,43 @@
+with counters as (
+    select  
+            content_id,
+            content_text,
+            length(content_text) - length(replace(content_text,' ', '')) as count_spaces,
+            length(content_text) - length(replace(content_text,'-', '')) as count_hyphen,
+            gs.i as ind,
+            substring(u.content_text from gs.i for 1) as char_
+    from user_content u 
+    cross join generate_series(1, length(u.content_text)) as gs(i))
+
+    , cte as (
+        select 
+                content_id, 
+                content_text, 
+                count_spaces,
+                count_hyphen,
+                ind,
+                char_,
+                lag(char_) over (partition by content_id
+                            order by ind) as prev_char
+        from counters
+    )
+    , cte2 as (
+        select 
+                content_id,
+                content_text,
+                case
+                    when prev_char is null or prev_char = ' ' 
+                        then upper(char_) 
+                    when prev_char = '-'
+                        then upper(char_)
+                    else lower(char_)
+                end as char
+        from cte
+    )
+
+    select  
+            content_id,
+            content_text as original_text,
+            string_agg(char, '') as converted_text
+    from cte2
+    group by 1, 2
