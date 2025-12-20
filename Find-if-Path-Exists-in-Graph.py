a1ed63f2b69969f680de63a8e609ffc15f4167d1
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-20 15:50:01 +0500

    [1971] [Time Beats: 93.17%] [Memory Beats: 99.98%] - LeetPush

--- Find-if-Path-Exists-in-Graph.py
+++ Find-if-Path-Exists-in-Graph.py
@@ -5,21 +5,19 @@
 5        
 6        graph = [[] for _ in range(n)]
 7        for u, v in edges:
-8            graph[u].append(v)
-9            graph[v].append(u)
+8            graph[v].append(u)
+9            graph[u].append(v)
 10        
-11        visited = [False] * n
-12        stack = [source]
+11        visited = [False] * n 
+12        q = deque([source])
 13        visited[source] = True
 14
-15        while stack:
-16            node = stack.pop()
+15        while q:
+16            node = q.popleft()
 17            if node == destination:
-18                return True 
-19            for neighbour in graph[node]:
-20                if not visited[neighbour]:
-21                    visited[neighbour] = True
-22                    stack.append(neighbour)
+18                return True
+19            for neighbor in graph[node]:
+20                if not visited[neighbor]:
+21                    visited[neighbor] = True
+22                    q.append(neighbor)
 23        return False
-24
-25
