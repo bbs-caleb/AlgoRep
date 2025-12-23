cf0c8e8ba08a10a93079ec54341b60654ba7c58b
bbs-caleb <babasaidovsaidgolib@gmail.com>
2025-12-23 09:16:34 +0500

    Time: 55 ms (36.35%) | Memory: 90.8 MB (44.61%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int x : nums) {
+            if (seen.find(x) != seen.end()) {
+                return true;
+            }
+            seen.insert(x);
+        }
+        return false;
+    }
+};
