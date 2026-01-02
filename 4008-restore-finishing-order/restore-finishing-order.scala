5bee095360a7b4aa409ccf24e0acdf91f82284f6
bbs-caleb <babasaidovsaidgolib@gmail.com>
2026-01-02 23:15:09 +0500

    Time: 20 ms (60.00%) | Memory: 60.3 MB (100.00%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.scala
+++ 4008-restore-finishing-order/restore-finishing-order.scala
@@ -1,6 +1,5 @@
 object Solution {
     def recoverOrder(order: Array[Int], friends: Array[Int]): Array[Int] = {
-        val friendsSet = friends.toSet
-        order.filter(friendsSet.contains)
+        order.filter(friends.contains)
     }
 }
