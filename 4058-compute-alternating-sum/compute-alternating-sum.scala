4ee94a2aa639ab503c270e8b7f60f1865b5ca9d5
bbs-caleb <babasaidovsaidgolib@gmail.com>
2026-01-02 23:08:56 +0500

    Time: 29 ms (13.33%) | Memory: 59.3 MB (86.67%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.scala
+++ 4058-compute-alternating-sum/compute-alternating-sum.scala
@@ -1,5 +1,5 @@
 object Solution {
     def alternatingSum(nums: Array[Int]): Int = {
-        nums.zipWithIndex.map {case (num, i) => if (i % 2 == 0) num else -num}.sum
+        nums.zipWithIndex.map{case (num, i) => if (i % 2 == 0) num else -num}.sum
     }
 }
