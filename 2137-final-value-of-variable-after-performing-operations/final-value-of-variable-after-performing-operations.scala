f0b0d5395bbf55e5b84b3269a7e84469e67a3be9
bbs-caleb <babasaidovsaidgolib@gmail.com>
2026-01-03 15:35:59 +0500

    Time: 9 ms (75.00%) | Memory: 59.8 MB (0.00%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.scala
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.scala
@@ -1,5 +1,11 @@
 object Solution {
     def finalValueAfterOperations(operations: Array[String]): Int = {
-        operations.foldLeft(0) {(x, op) => if (op.contains("+")) x + 1 else x - 1}
+        var x = 0
+        for (op <- operations) {
+            if (op.contains("+")) x += 1 
+            else x -= 1
+        }
+
+        x
     }
 }
